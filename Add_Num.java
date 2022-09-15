5137acb3ac0ec9bb5aa0065d0af080dea1e8afc8
nasir-buddy <syedali172000@gmail.com>
2022-09-16 00:07:38 +0530

    Add numbers program

---
+++ Add_Num.java
@@ -0,0 +1,12 @@
+package Normal_programs;
+public class Add_Num 
+{   
+    public static void main(String execute[])
+    {
+        int i = 5;
+        int j = 10;
+        int k = i + j;
+        System.out.println("The addition of the value is ");
+        System.out.println(k);
+    }
+}
